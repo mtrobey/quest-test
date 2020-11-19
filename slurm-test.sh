0095c2b7529629458fa46ad314173a35dcf60370
mtrobey <mattrobey@gmail.com>
2020-11-19 10:45:35 -0600

    fixed email type

--- slurm-test.sh
+++ slurm-test.sh
@@ -5,9 +5,8 @@
 #SBATCH -N 1                    # Number of Nodes
 #SBATCH --mem=18G               # Memory per node in GB needed for a job. Also see --mem-per-cpu
 #SBATCH --ntasks-per-node=6     # Number of Cores (Processors)
-#SBATCH --mail-user=<my_email>  # Designate email address for job communications
-#SBATCH --mail-type=<event>     # Events options are job BEGIN, END, NONE, FAIL, REQUEUE
-#SBATCH --output=<file_path>    # Path for output must already exist
-#SBATCH --error=<file_path>     # Path for errors must already exist
+#SBATCH --mail-user=mattrobey@gmail.com  # Designate email address for job communications
+#SBATCH --mail-type=BEGIN     # Events options are job BEGIN, END, NONE, FAIL, REQUEUE
+#SBATCH --output=test.txt    # Path for output must already exist
 #SBATCH --job-name="test"       # Name of job
 echo "I just executed!"
